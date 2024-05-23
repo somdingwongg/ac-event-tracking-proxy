a92b62a313b3f0b5c94572c7ea27a11487a6419a
somdingwongg <121094232+somdingwongg@users.noreply.github.com>
2024-05-23 14:38:24 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 const BASE_URL = process.env.BASE_URL || `http://localhost:${PORT}`;
 
 console.log('Starting application...');
